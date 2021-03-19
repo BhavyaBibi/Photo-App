77beccda4909a84cd73a3e4faf643d28a1e3e18e
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-03-19 08:09:32 +0400

    images  views section cleared add photoby database

--- photos/views.py
+++ photos/views.py
@@ -4,12 +4,15 @@ from .models import Category, Photo
 # Create your views here.
 def gallery(request):
     categories = Category.objects.all()
-    context = {'categories': categories}
+    photos = Photo.objects.all()
+    context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html',context)
   
 def viewPhoto(request,pk):
-    categories = Category.objects.all()
-    context = {'categories': categories}
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
+
+ 
     return render(request,'photos/photo.html')
 
 def addPhoto(request):
